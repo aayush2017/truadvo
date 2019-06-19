c7ba7a2180499873a0febce7e54b37ef1b40074a
aayush2017 <aayush.jaiswal984@gmail.com>
2019-06-19 15:56:14 +0530

    Create ParterClient Page - model and Detail Tab Only

--- Partner-Portal/PartnerClient.php
+++ Partner-Portal/PartnerClient.php
@@ -78,6 +78,110 @@
 		</div>
 	</div>	
 </div>
+<div class="clr-fix"></div>
+<div class="partner-client-details">
+	<div class="partner-client-details-tab">
+		<div class="pcd-tab-button">
+			<button class="activeBtn">Details</button>
+			<button>Works</button>
+			<button>Payments</button>
+			<button>All Docs</button>
+			<button>Communication</button>
+			<button id="close"><i class="fas fa-times"></i></button>
+		</div>
+		<div class="pcd-tab-content">
+			<div class="details-content">
+				<div class="left-content">
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>WorkID</label>
+							<span>#TA009780951</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Joining date</label>
+							<span>04-June-2019</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Email ID</label>
+							<span>client@website.com</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Phone Number</label>
+							<span>+91 9876543210</span>
+						</div>
+					</div>
+				</div>
+				<div class="right-content">
+					<div class="rc-box1">
+						<div class="rcb-data">
+							<div class="rcb-data-text">
+								<label>Customer Consultation Form</label>
+								<span>#TA009780951</span>
+							</div>
+							<div class="rcb-data-icon">
+								<i class="fas fa-file-alt"></i>
+							</div>
+						</div>
+						
+						<div class="rcb-data">
+							<div class="rcb-data-text">
+								<label>Estimated Date of Completion (If Any)</label>
+								<span>10-August-2019</span>
+							</div>
+						</div>
+						<div class="rcb-data">
+							<div class="rcb-data-text">
+								<label>Name of oppsing Party (If any)</label>
+								<span>Mojender S.</span>
+							</div>
+						</div>
+						<div class="rcb-data">
+							<div class="rcb-data-text">
+								<label>Contact Details of Opposing Party (If any)</label>
+								<span>+91 9876543210</span>
+							</div>
+						</div>
+
+					</div>
+					<div class="rc-box2">
+						<div class="rcb-data">
+							<div class="rcb-data-text">
+								<label>Team Members Apponted</label>
+								<span>Kandarp G.,</span>
+								<span>Aashna S.,</span>
+								<span>Rohit S.,</span>
+								<span>M.S Dhoni,</span>
+							</div>
+						</div>
+					</div>
+					<div class="rc-up-down">
+						<div class="rc-up"><i class="fas fa-chevron-up"></i></div>
+						<div class="rc-down"><i class="fas fa-chevron-down"></i></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php require_once('include/footer.php'); ?>
+<script>
+$(document).ready(function() {
+	$(".folderView").click(function() {
+		$(".partner-client-details").css("display", "block");
+	});
+	$("#close").click(function() {
+		$(".partner-client-details").css("display", "none");
+	});
+	$(".pcd-tab-button button").click(function() {
+
+	});
+});
+</script>
 </body>
 </html>
