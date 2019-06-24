dd5b0873b13e3303e913e4ee15548079690f3793
aayush2017 <aayush.jaiswal984@gmail.com>
2019-06-24 14:49:55 +0530

    Client detail tab update only work pendding

--- Partner-Portal/PartnerClient.php
+++ Partner-Portal/PartnerClient.php
@@ -82,15 +82,15 @@
 <div class="partner-client-details">
 	<div class="partner-client-details-tab">
 		<div class="pcd-tab-button">
-			<button class="activeBtn">Details</button>
-			<button>Works</button>
-			<button>Payments</button>
-			<button>All Docs</button>
-			<button>Communication</button>
+			<button class="activeBtn" data-btnName="details">Details</button>
+			<button data-btnName="work">Works</button>
+			<button data-btnName="payment">Payments</button>
+			<button data-btnName="all-docs">All Docs</button>
+			<button data-btnName="communication">Communication</button>
 			<button id="close"><i class="fas fa-times"></i></button>
 		</div>
 		<div class="pcd-tab-content">
-			<div class="details-content">
+			<div class="details-content activeTab" id="details">
 				<div class="left-content">
 					<div class="rcb-data">
 						<div class="rcb-data-text">
@@ -165,7 +165,234 @@
 						<div class="rc-down"><i class="fas fa-chevron-down"></i></div>
 					</div>
 				</div>
-			</div>
+			</div>	<!-- Detail Content end -->
+			<div class="details-content clr-fix" id="work">
+				<div class="left-content">
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>WorkID</label>
+							<span>#TA009780951</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Joining date</label>
+							<span>04-June-2019</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Client Name</label>
+							<span>J.B Reddy</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Email ID</label>
+							<span>client@website.com</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Phone Number</label>
+							<span>+91 9876543210</span>
+						</div>
+					</div>
+				</div>
+				<div class="right-content">
+					<h1>Progress Bar</h1>
+				</div>
+			</div>	<!-- Detail Content - work End -->
+			<div class="details-content clr-fix" id="payment">
+				<div class="left-content">
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>WorkID</label>
+							<span>#TA009780951</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Joining date</label>
+							<span>04-June-2019</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Email ID</label>
+							<span>client@website.com</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Phone Number</label>
+							<span>+91 9876543210</span>
+						</div>
+					</div>
+				</div>
+				<div class="right-content">
+					<div class="rc-box1">
+						<div class="rcb-data">
+							<div class="rcb-data-text">
+								<label>Payments Made By Clients</label>
+								<span><i class="fas fa-rupee-sign"></i> 23,500</span>
+							</div>
+						</div>
+						<br/><br/>
+						<div class="rcb-data">
+							<div class="rcb-data-text">
+								<label>Payments Requested By Professional</label>
+								<span><i class="fas fa-rupee-sign"></i> 23,500</span>
+							</div>
+						</div>
+						<br/><br/>
+							<button class="parTaskBtn active">Request Invoice</button>
+					</div>
+				</div>
+			</div>	<!-- Detail Content Payment end -->
+			<div class="details-content clr-fix" id="all-docs">
+				<div class="left-content">
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>WorkID</label>
+							<span>#TA009780951</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Joining date</label>
+							<span>04-June-2019</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Email ID</label>
+							<span>client@website.com</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Phone Number</label>
+							<span>+91 9876543210</span>
+						</div>
+					</div>
+				</div>
+				<div class="right-content">
+					<button class="parTaskBtn active"><i class="fas fa-upload" style=""></i> <span>Upload</span></button>
+					<br/><br/>
+					<div class="partner-client-details-table">
+						<div class="mltr">
+							<table>
+								<thead>
+									<th>Date of Upload</th>
+									<th>Work Related to</th>
+									<th>Manage</th>
+								</thead>
+								<tbody>
+									<tr>
+										<td class="border-megneta">01-06-2019</td>
+										<td>GST Filling</td>
+										<td>
+											<a href=""><i class="fas fa-trash-alt"></i></a>
+											<a href=""><i class="fas fa-share-alt"></i></a>
+										</td>
+									</tr>
+									<tr>
+										<td class="border-green">01-06-2019</td>
+										<td>Litigation</td>
+										<td>
+											<a href=""><i class="fas fa-trash-alt"></i></a>
+											<a href=""><i class="fas fa-share-alt"></i></a>
+										</td>
+									</tr>
+									<tr class="border-megneta">
+										<td>01-06-2019</td>
+										<td>GST Filling</td>
+										<td>
+											<a href=""><i class="fas fa-trash-alt"></i></a>
+											<a href=""><i class="fas fa-share-alt"></i></a>
+										</td>
+									</tr>
+									<tr class="border-green">
+										<td>01-06-2019</td>
+										<td>Litigation</td>
+										<td>
+											<a href=""><i class="fas fa-trash-alt"></i></a>
+											<a href=""><i class="fas fa-share-alt"></i></a>
+										</td>
+									</tr>
+									<tr class="border-green">
+										<td>01-06-2019</td>
+										<td>Litigation</td>
+										<td>
+											<a href=""><i class="fas fa-trash-alt"></i></a>
+											<a href=""><i class="fas fa-share-alt"></i></a>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>	
+					</div>
+					<div class="rc-up-down">
+						<div class="rc-up"><i class="fas fa-chevron-up"></i></div>
+						<div class="rc-down"><i class="fas fa-chevron-down"></i></div>
+					</div>
+				</div>
+			</div>	<!-- Detail Content All Docs end -->
+			<div class="details-content clr-fix" id="communication">
+				<div class="left-content">
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>WorkID</label>
+							<span>#TA009780951</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Joining date</label>
+							<span>04-June-2019</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Email ID</label>
+							<span>client@website.com</span>
+						</div>
+					</div>
+					<div class="rcb-data">
+						<div class="rcb-data-text">
+							<label>Phone Number</label>
+							<span>+91 9876543210</span>
+						</div>
+					</div>
+				</div>
+				<div class="right-content">
+					<div class="chat">
+						<div class="right-content-send">
+							<div class="pic-circle"></div>
+							<div class="message"></div>
+						</div>
+						<div class="right-content-recived">
+							<div class="message"></div>
+							<div class="pic-circle"></div>
+						</div>
+						<br/><br/>
+						<br/><br/>
+						<div class="right-content-send-msg">
+							<i>!</i>
+							<i>!!</i>
+							<i>!!!</i>
+							<input type="text" name="" placeholder="Write your message">
+							<i class="fas fa-paperclip"></i>
+							<i class="fas fa-paper-plane"></i>
+						</div>
+					</div>
+					<div class="rc-up-down">
+							<div class="rc-up"><i class="fas fa-chevron-up"></i></div>
+							<div class="rc-down"><i class="fas fa-chevron-down"></i></div>
+						</div>
+				</div>
+			</div>	<!-- Detail Content Communication end -->
 		</div>
 	</div>
 </div>
@@ -178,8 +405,40 @@ $(document).ready(function() {
 	$("#close").click(function() {
 		$(".partner-client-details").css("display", "none");
 	});
-	$(".pcd-tab-button button").click(function() {
+	$(".pcd-tab-button button:nth-child(1)").click(function() {
+		$(".pcd-tab-button button").removeClass("activeBtn");
+		$(".details-content").removeClass("activeTab");
 
+		$(".pcd-tab-button button:nth-child(1)").addClass("activeBtn");
+		$("#details").addClass("activeTab");
+	});
+	$(".pcd-tab-button button:nth-child(2)").click(function() {
+		$(".pcd-tab-button button").removeClass("activeBtn");
+		$(".details-content").removeClass("activeTab");
+
+		$(".pcd-tab-button button:nth-child(2)").addClass("activeBtn");
+		$("#work").addClass("activeTab");
+	});
+	$(".pcd-tab-button button:nth-child(3)").click(function() {
+		$(".pcd-tab-button button").removeClass("activeBtn");
+		$(".details-content").removeClass("activeTab");
+
+		$(".pcd-tab-button button:nth-child(3)").addClass("activeBtn");
+		$("#payment").addClass("activeTab");
+	});
+	$(".pcd-tab-button button:nth-child(4)").click(function() {
+		$(".pcd-tab-button button").removeClass("activeBtn");
+		$(".details-content").removeClass("activeTab");
+
+		$(".pcd-tab-button button:nth-child(4)").addClass("activeBtn");
+		$("#all-docs").addClass("activeTab");
+	});
+	$(".pcd-tab-button button:nth-child(5)").click(function() {
+		$(".pcd-tab-button button").removeClass("activeBtn");
+		$(".details-content").removeClass("activeTab");
+
+		$(".pcd-tab-button button:nth-child(5)").addClass("activeBtn");
+		$("#communication").addClass("activeTab");
 	});
 });
 </script>
